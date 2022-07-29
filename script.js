f6e05ae42674891a8e0148b8f30d9246ffd086e5
Brian Lai <laivbrian@gmail.com>
2022-07-29 13:38:07 -0500

    Add calculator display and numbers display on button click

--- script.js
+++ script.js
@@ -37,3 +37,63 @@ function operate(operator, a, b) {
         return divide(a, b);
     }
 }
+
+let displayedNumber = "";
+
+function displayNumber(number) {
+    displayedNumber += number;
+    calculatorDisplay.textContent = `${displayedNumber}`;
+}
+
+// UI
+const calculatorDisplay = document.querySelector('#display');
+const oneBtn = document.querySelector('#one');
+const twoBtn = document.querySelector('#two');
+const threeBtn = document.querySelector('#three');
+const fourBtn = document.querySelector('#four');
+const fiveBtn = document.querySelector('#five');
+const sixBtn = document.querySelector('#six');
+const sevenBtn = document.querySelector('#seven');
+const eightBtn = document.querySelector('#eight');
+const nineBtn = document.querySelector('#nine');
+const zeroBtn = document.querySelector('#zero');
+
+oneBtn.addEventListener('click', () => {
+    displayNumber("1");
+});
+
+twoBtn.addEventListener('click', () => {
+    displayNumber("2");
+});
+
+threeBtn.addEventListener('click', () => {
+    displayNumber("3");
+});
+
+fourBtn.addEventListener('click', () => {
+    displayNumber("4");
+});
+
+fiveBtn.addEventListener('click', () => {
+    displayNumber("5");
+});
+
+sixBtn.addEventListener('click', () => {
+    displayNumber("6");
+});
+
+sevenBtn.addEventListener('click', () => {
+    displayNumber("7");
+});
+
+eightBtn.addEventListener('click', () => {
+    displayNumber("8");
+});
+
+nineBtn.addEventListener('click', () => {
+    displayNumber("9");
+});
+
+zeroBtn.addEventListener('click', () => {
+    displayNumber("0");
+});
