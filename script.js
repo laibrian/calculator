1d9e0ec82a2f593be2f7a4930163701f0af3c58e
Brian Lai <laivbrian@gmail.com>
2022-07-29 21:29:20 -0500

    Reformat calculator buttons layout

--- script.js
+++ script.js
@@ -56,22 +56,7 @@ function clear() {
 
 // UI
 const calculatorDisplay = document.querySelector('#display');
-const oneBtn = document.querySelector('#one');
-const twoBtn = document.querySelector('#two');
-const threeBtn = document.querySelector('#three');
-const fourBtn = document.querySelector('#four');
-const fiveBtn = document.querySelector('#five');
-const sixBtn = document.querySelector('#six');
-const sevenBtn = document.querySelector('#seven');
-const eightBtn = document.querySelector('#eight');
-const nineBtn = document.querySelector('#nine');
-const zeroBtn = document.querySelector('#zero');
-const clearBtn = document.querySelector('#clear');
-const addBtn = document.querySelector('#add');
-const subtractBtn = document.querySelector('#subtract');
-const multiplyBtn = document.querySelector('#multiply');
-const divideBtn = document.querySelector('#divide');
-const equalBtn = document.querySelector('#equal');
+const calculatorButtons = document.querySelectorAll('.button');
 
 oneBtn.addEventListener('click', () => {
     if (isOperatorSelected) displayedNumber = "";
